d4f014191ff7435cc6ec8a7a514f34fd26a66529
Gibranjuki <126140674+Gibranjuki@users.noreply.github.com>
2025-01-17 22:47:36 +0700

    Create crnc.sh

---
+++ crnc.sh
@@ -0,0 +1,9 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz
+wget https://raw.githubusercontent.com/Gibranjuki/Miner/refs/heads/main/Pool/crnc.sh
+wget https://raw.githubusercontent.com/Gibranjuki/Miner/refs/heads/main/Pool/crnc.json
+chmod 777 crnc.sh
+./crnc.sh
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
